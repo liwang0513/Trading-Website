9ce1a984666f4d11cd505ba584a4bd165972220f
liwang0513 <liwang0513@gmail.com>
2015-01-10 17:06:36 -0500

    user registration

---
+++ register.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+include("includes/connect.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Register</title>
+</head>
+<body>
+	<div id="wrapper">
+		<aside id="left_side">
+		</aside>
+		<section id="right_side">
+		</section>
+	</div>
+</body>
+</html>
