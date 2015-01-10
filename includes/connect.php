a74973d76fa20c669a3a205f101ed469b8d04c8f
liwang0513 <liwang0513@gmail.com>
2015-01-10 16:58:05 -0500

    connect to the database

---
+++ includes/connect.php
@@ -0,0 +1,12 @@
+<?php
+$connect = mysql_connect('localhost','bucky_name','TSXG9GJWNn96pba5');
+
+	die('Could not connect '.mysql_error());
+}
+
+$db_selected = mysql_select_db('bucky_lisa');
+if (!$db_selected) {
+	die('Could not select database'.mysql_error());
+}
+
+?>
