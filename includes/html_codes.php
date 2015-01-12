a801854670963bac7b5d8e15ab5d42f70d4aee2e
liwang0513 <liwang0513@gmail.com>
2015-01-12 18:30:16 -0500

    top search bar

--- includes/html_codes.php
+++ includes/html_codes.php
@@ -12,7 +12,21 @@ function headerAndSearcgCode() {
 	echo "
 			</div>
 			<a href=\"index.php\"><img src=\"images/mainLogo.png\"></a>
+			
+			<div id=\"top_search\">
+			<form name=\"input\" action=\"search.php\" method=\"get\">
+				<input type=\"text\" id=\"keywords\" name=\"keywords\" size=\"100\" class=\"searchBox\" value=\"$defaultText\"> &nbsp;
+				<select id=\"category\" name=\"category\" class=\"searchBox\">
 	";
+	
+	//include categories here
+	
+	echo '
+				</select>
+				<input type="submit" value="Search" class="button" />
+			</form>
+			</div>
+	';
 } 	
 	
 ?>
